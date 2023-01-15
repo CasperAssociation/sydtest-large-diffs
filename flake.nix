53da533d672b97e92205776c02a8eb8b54ba417e
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2023-01-15 15:00:18 -0500

    use updated sydtest

--- flake.nix
+++ flake.nix
@@ -7,10 +7,7 @@
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     horizon-platform = {
-      url = "git+https://gitlab.homotopic.tech/horizon/horizon-platform";
-    };
-    sydtest = {
-      url = "git+https://github.com/NorfairKing/sydtest?ref=faster-diffs";
+      url = "git+https://github.com/morganthomas/horizon-platform?sydtest-diffs";
     };
   };
   outputs =
@@ -19,19 +16,18 @@
     , flake-utils
     , horizon-platform
     , nixpkgs
-    , sydtest
-    , ...
     }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
     let
-      pkgs = import nixpkgs { inherit system; overlays = [sydtest.overlays.x86_64-linux]; };
+      pkgs = import nixpkgs { inherit system; };
       hsPkgs =
         with pkgs.haskell.lib;
         pkgs.haskell.packages.ghc942.override
           {
             overrides = hfinal: hprev:
-              horizon-platform.packages.x86_64-linux // {
-                sydtest-large-diffs-spec = disableLibraryProfiling (hprev.callCabal2nix "osl:spec" ./. { });
+              {
+                sydtest-large-diffs-spec = disableLibraryProfiling (hprev.callCabal2nix "osl:spec" ./. {
+                });
               };
           };
     in
